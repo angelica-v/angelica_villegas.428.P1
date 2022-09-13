444e99c2576e77fb6ae12473de7b2bac60293b53
angelica-v <angelicadv10@gmail.com>
2022-09-13 15:08:43 -0500

    Added mountains + coins

---
+++ Assets/kartMove.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kartMove : MonoBehaviour
+{
+    // float timeCounter = 0;
+     
+ 
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+         
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       /* timeCounter += Time.deltaTime; 
+
+        float x =  -0.0678f + 0.02f * Mathf.Sin(timeCounter);
+        float y = 0.055f;
+        float z = 0.015f + 0.02f * Mathf.Cos(timeCounter);
+        transform.position = new Vector3(x, y, z);*/
+    }
+}
